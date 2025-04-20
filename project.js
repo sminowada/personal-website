d088d6efc42f8e7cacaea8b479324c1b9daa9e40
sminowada <97696723+sminowada@users.noreply.github.com>
2025-04-19 20:35:51 -0700

    removed local/remote

--- project.js
+++ project.js
@@ -1,18 +1,3 @@
-async function getData() {
-    const url = "https://my-json-server.typicode.com/sminowada/personal-website/projects";
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-
-        const json = await response.json();
-        return json;
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
 class ProjectCard extends HTMLElement {
     constructor({ title = 'Project Title', description = 'This is a description of the project.',
         link = "",
@@ -43,22 +28,6 @@ class ProjectCard extends HTMLElement {
 }
 
 customElements.define('project-card', ProjectCard);
-// let card1 = new ProjectCard({
-//     title: 'Ensemble Learning',
-//     description: 'Ensemble learning models using Hyper-Dimensional Computing for use on edge devices. Achieved greater energy efficiency and high accuracy on image classification datasets such as mnist and cifar',
-//     link: 'https://ieeexplore.ieee.org/abstract/document/10818094',
-//     linkDescription: 'IEEE Paper',
-//     picture: "inference_proposal.png",
-//     altText: "image of ensemble learning using HDC"
-// });
-// let card2 = new ProjectCard({
-//     title: 'Pantry Pal',
-//     description: 'Developed an application for generating recipes with pictures based on user voice input ingredients. Implemented API calls for recipe creation, image generation, and speech recognition. Validated logins and stored user profiles with recipes using MongoDB',
-//     link: 'https://github.com/ucsd-cse110-fa23/cse-110-project-team_3?tab=readme-ov-file',
-//     linkDescription: 'Github Repo',
-//     picture: "pantrypal.png",
-//     alText: "pantry pal logo image"
-// });
 
 const projectOne = {
     title: 'Ensemble Learning',
@@ -77,29 +46,54 @@ const projectTwo = {
     altText: "pantry pal logo image"
 };
 
-localStorage.setItem("projectOne", JSON.stringify(projectOne));
-localStorage.setItem("projectTwo", JSON.stringify(projectTwo));
+let p1 = projectOne;
+let p2 = projectTwo;
+let card1 = new ProjectCard(p1);
+let card2 = new ProjectCard(p2);
+card1.classList.add('card');
+card2.classList.add('card');
+document.querySelector('main').innerHTML = '';
+document.querySelector('main').prepend(card2);
+document.querySelector('main').prepend(card1);
 
-document.getElementById('localBtn').addEventListener('click', function () {
-    let p1 = JSON.parse(localStorage.getItem("projectOne"));
-    let p2 = JSON.parse(localStorage.getItem("projectTwo"));
-    let card1 = new ProjectCard(p1);
-    let card2 = new ProjectCard(p2);
-    card1.classList.add('card');
-    card2.classList.add('card');
-    document.querySelector('main').innerHTML = '';
-    document.querySelector('main').prepend(card2);
-    document.querySelector('main').prepend(card1);
+// localStorage.setItem("projectOne", JSON.stringify(projectOne));
+// localStorage.setItem("projectTwo", JSON.stringify(projectTwo));
 
-});
+// async function getData() {
+//     const url = "https://my-json-server.typicode.com/sminowada/personal-website/projects";
+//     try {
+//         const response = await fetch(url);
+//         if (!response.ok) {
+//             throw new Error(`Response status: ${response.status}`);
+//         }
 
-document.getElementById("remoteBtn").addEventListener('click', async function () {
-    let projects = await getData();
-    let card1 = new ProjectCard(projects[0]);
-    let card2 = new ProjectCard(projects[1]);
-    card1.classList.add('card');
-    card2.classList.add('card');
-    document.querySelector('main').innerHTML = '';
-    document.querySelector('main').prepend(card2);
-    document.querySelector('main').prepend(card1);
-});
+//         const json = await response.json();
+//         return json;
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// }
+
+// document.getElementById('localBtn').addEventListener('click', function () {
+//     let p1 = JSON.parse(localStorage.getItem("projectOne"));
+//     let p2 = JSON.parse(localStorage.getItem("projectTwo"));
+//     let card1 = new ProjectCard(p1);
+//     let card2 = new ProjectCard(p2);
+//     card1.classList.add('card');
+//     card2.classList.add('card');
+//     document.querySelector('main').innerHTML = '';
+//     document.querySelector('main').prepend(card2);
+//     document.querySelector('main').prepend(card1);
+
+// });
+
+// document.getElementById("remoteBtn").addEventListener('click', async function () {
+//     let projects = await getData();
+//     let card1 = new ProjectCard(projects[0]);
+//     let card2 = new ProjectCard(projects[1]);
+//     card1.classList.add('card');
+//     card2.classList.add('card');
+//     document.querySelector('main').innerHTML = '';
+//     document.querySelector('main').prepend(card2);
+//     document.querySelector('main').prepend(card1);
+// });
