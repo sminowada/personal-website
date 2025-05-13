bbb6f65940621cdb27dc40a9009b66764a5c09cf
sminowada <97696723+sminowada@users.noreply.github.com>
2025-05-12 19:55:57 -0700

    summary. update

--- about.js
+++ about.js
@@ -15,25 +15,26 @@ function showSlides(n) {
     const slides = document.getElementsByClassName("mySlides");
     const dots = document.getElementsByClassName("dot");
 
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
 
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].classList.remove("active");
     }
 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].classList.add("active");
     dots[slideIndex - 1].className += " active";
 }
 
-// Auto Slide every 3 seconds
+
+// Auto Slide every 5 seconds
 let autoSlide = setInterval(() => {
     plusSlides(1);
-}, 3000);
+}, 5000);
 
 // Optional: Pause on hover
 const slideshowContainer = document.querySelector('.slideshow-container');
